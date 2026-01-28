c9231be719d1651f01492caaa54073ded1a8f84a
Jesse Mathis <jmathis7@radar.gsw.edu>
2026-01-28 11:57:03 -0500

    changed back and redeployed Azure function as windows app.

--- backend/index.js
+++
@@ -1,6 +0,0 @@
-const { app } = require("@azure/functions");
-
-app.setup({ enableHttpStream: true });
-
-// load all functions through one file
-require("./src");
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,3 @@ const { app } = require('@azure/functions');
 app.setup({
     enableHttpStream: true,
 });
-require("./functions/startRoute");
-require("./functions/sendRoute");
-require("./functions/uploadPoints");
-

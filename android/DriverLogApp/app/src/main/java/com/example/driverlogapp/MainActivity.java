c8b2aff9e4fc5632100d4bec0810a88263fddefe
StudentWorker835 <ajorda14@radar.gsw.edu>
2026-02-05 09:31:33 -0500

    Continued Dev, building framework and start/stop blocks.

--- android/DriverLogApp/app/src/main/java/com/example/driverlogapp/MainActivity.java
+++ android/DriverLogApp/app/src/main/java/com/example/driverlogapp/MainActivity.java
@@ -14,12 +14,19 @@ import android.widget.*;
 import com.google.android.gms.location.*;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.io.IOException;
+
+import okhttp3.*;
+
 public class MainActivity extends AppCompatActivity {
 
     // Declare variables
+    private int routeID;
+    private boolean loopFinished;
+
     private Button getLocationBtn;
     private FusedLocationProviderClient locationClient;
-    private TextView locationText;
+    private TextView editText;
     private static final int LOCATION_PERMISSION_REQUEST = 1001;
     private boolean isRunning = false;
 
@@ -46,6 +53,10 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST);
             return;
         }
+
+        // initialize variables
+        routeID = 0;
+
         //If block controlling start stopping route and button display
         if (!isRunning) {
             isRunning = true;
@@ -53,14 +64,54 @@ public class MainActivity extends AppCompatActivity {
             getLocationBtn.setBackgroundColor(Color.RED);
             Toast.makeText(this, "Route Started", Toast.LENGTH_SHORT).show();
             //Send blank JSON file with expected headers to backend to start recording route on that end
+            routeID = startRoute()[0];
+            Toast.makeText(this, "Route ID: " + routeID, Toast.LENGTH_SHORT).show();
         }
         else {
             isRunning = false;
             getLocationBtn.setText("Start");
             getLocationBtn.setBackgroundColor(Color.parseColor("#246B19"));
             Toast.makeText(this, "Route Stopped", Toast.LENGTH_SHORT).show();
-            //Send blank JSON file with expected headers to backend to stop recording route on that end
         }
+
+        //While/For loop controlling the polling and logging of location data
+        while (isRunning) {
+            //early escape check
+            loopFinished = false;
+
+            for (int i = 0; i < 30; i++) {
+                // Fetch the last known location
+                locationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            //Add location information to JSON file
+
+                        }
+                    }
+                });
+
+                //timer so the loop doesn't run too fast
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            //Send populated JSON file to backend and repeat
+            loopFinished = true;
+        }
+
+        //if block to check is JSON file exists and to send it to backend
+        if (!loopFinished) {
+
+        }
+
+        //Send blank JSON file with expected headers to backend to stop recording route on that end
+        stopRoute();
+
 
 
 
@@ -75,21 +126,53 @@ public class MainActivity extends AppCompatActivity {
                     // Get latitude and longitude
                     double lat = location.getLatitude();
                     double lon = location.getLongitude();
-                    Toast.makeText(MainActivity.this, "location success", Toast.LENGTH_SHORT).show();
 
                     // Display location in TextView
-                    locationText.setText("Latitude: " + lat + "\nLongitude: " + lon);
-                }
-                else if (location == null) {
-                    // Display error message if location is null
-                    locationText.setText("Unable to get location");
-                }
-                else {
-                    locationText.setText("Something is wrong");
                 }
             }
         });
     }
+
+    public int[] startRoute() {
+        final int[] routeID = {0};
+        new Thread(() -> {
+            try {
+                OkHttpClient client = new OkHttpClient();
+
+                RequestBody body = RequestBody.create("{}", MediaType.parse("application/json"));
+                Request request = new Request.Builder()
+                        .url("https://driverlogbackend-cwe7gpeuamfhffgt.eastus-01.azurewebsites.net/api/routes/start")
+                        .post(body)
+                        .build();
+                Response response = client.newCall(request).execute();
+                if (response.isSuccessful()) {
+                    routeID[0] = response.code();
+                }
+                } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        return routeID;
+    }
+
+    public void stopRoute() {
+        new Thread(() -> {
+            try {
+                OkHttpClient client = new OkHttpClient();
+
+                RequestBody body = RequestBody.create("{}", MediaType.parse("application/json"));
+                Request request = new Request.Builder()
+                        .url("https://driverlogbackend-cwe7gpeuamfhffgt.eastus-01.azurewebsites.net/api/routes/r-50f935f3-a637-4562-af78-56fd5323fc57/end")
+                        .post(body)
+                        .build();
+                Response response = client.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+                }
+        });
+
+    }
+
 
     // Handle the result of the permission request
     @Override

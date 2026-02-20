c7682168325baa086a5e149b605167e11442dd23
Jesse Mathis <jmathis7@radar.gsw.edu>
2026-02-20 04:42:27 -0500

    updated auth.js to newer config for getting user information from header

--- backend/shared/auth.js
+++ backend/shared/auth.js
@@ -1,15 +1,25 @@
 function getUserFromRequest(req) {
-  const header = req.headers["x-ms-client-principal"];
+
+  const header = req.headers.get("x-ms-client-principal");
   if (!header) return null;
 
   const decoded = Buffer.from(header, "base64").toString("utf8");
   const principal = JSON.parse(decoded);
 
+  const claims = principal.claims || [];
+
+  const getClaim = (type) =>
+    claims.find(c => c.typ === type)?.val;
+
   return {
-    userId: principal.userId,
-    username: principal.userDetails,
-    identityProvider: principal.identityProvider,
-    claims: principal.claims
+    userId: getClaim(
+      "http://schemas.microsoft.com/identity/claims/objectidentifier"
+    ),
+    username: getClaim(
+      "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"
+    ),
+    identityProvider: principal.auth_typ,
+    claims
   };
 }
 
